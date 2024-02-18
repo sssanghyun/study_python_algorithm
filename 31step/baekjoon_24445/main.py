295c8bc1de680b73056bbee8e43dc6e0b77446d3
ssang2027 <o_o2027@naver.com>
2024-02-18 16:15:51 +0900

    백준 31step 그래프와 순회 | 1~4 문제 완료

---
+++ 31step/baekjoon_24445/main.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+N, M, R = map(int, sys.stdin.readline().split(' '))
+node = [[] for _ in range(N+1)]
+visited = [0 for _ in range(N+1)]
+q = deque()
+
+for i in range(M):
+    u, v = map(int, sys.stdin.readline().split(' '))
+    node[u].append(v)
+    node[v].append(u)
+
+for i in range(1, N+1):
+    node[i].sort(key=lambda x: -x)
+count = 1
+
+visited[R] = count
+q.append(R)
+
+while len(q) > 0:
+    a = q.popleft()
+    for k in node[a]:
+        if visited[k] == 0:
+            count += 1
+            visited[k] = count
+            q.append(k)
+
+for i in range(1, N+1):
+    print(visited[i])
---
+++ 31step/baekjoon_24479/main.py
@@ -0,0 +1,25 @@
+import sys
+
+N, M, R = map(int, sys.stdin.readline().split(' '))
+node = [[] for _ in range(N+1)]
+visited = [0 for _ in range(N+1)]
+sys.setrecursionlimit(N+10)
+for i in range(M):
+    u, v = map(int, sys.stdin.readline().split(' '))
+    node[u].append(v)
+    node[v].append(u)
+
+for i in range(1, N+1):
+    node[i].sort()
+count = 0
+def dfs(r):
+    global count
+    count += 1
+    visited[r] = count
+    for i in node[r]:
+        if not visited[i]:
+            dfs(i)
+dfs(R)
+
+for i in range(1, N+1):
+    print(visited[i])
